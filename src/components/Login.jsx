ea6bae72e4b3a1031056b54e50c07bd2e31c0af9
lMehefuzl <khondakermehefuz@gmail.com>
2026-06-23 16:42:47 +0200

    login fait mais coté clien on voit aucune diff pour le moment

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,126 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-return (
-    <form action="" className="container-fluid d-flex justify-content-center w-50 bg-light border border-primary border-5 rounded-5 shadow-lg my-4">
-    <div className="rounded-5 py-4 px-5 w-100 ">
-        <h1 className="card-title h1 m-3 border-bottom border-primary border-4 text-center text-primary"><b>Connexion</b></h1>
-        <div className="row mx-4 my-5">
-        <div className="col-md-12">
-            <label className="form-label" htmlFor="email">Adresse email</label>
-            <input className="form-control border border-secondary border-3 rounded-3" type="email" id="email" name="email" placeholder="Entrez votre email" />
-        </div>
-        <div className="col-md-12 mt-4">
-            <label className="form-label" htmlFor="password">Mot de passe</label>
-            <input className="form-control border border-secondary border-3 rounded-3" type="password" id="password" name="password" placeholder="Entrez votre mot de passe" />
-        </div>
-        <div className="col-md-12 mt-4">
-        </div>
-        <div className="d-flex flex-column flex-md-row gap-3 justify-content-between">
-            <button className="btn btn-primary btn-lg rounded-5 px-4 flex-fill" type="submit">Valider</button>
-        </div>
-        </div>
-    </div>
-    </form>
-  )
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const [errors, setErrors] = useState({})
+    const [valid, setValid] = useState(true)
+    const navigate = useNavigate()
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    let isvalid = true;
+    let validationErrors = {}
+
+    if (formData.email === "" || formData.email === null) {
+        isvalid = false;
+        validationErrors.email = "Veuillez mettre votre mail"
+    } else if(!/\S+@\S+\.\S+/.test(formData.email)) {
+        isvalid = false;
+        validationErrors.email = "Votre mail n'est pas valide"
+    }
+
+    if (formData.password === "" || formData.password === null) {
+        isvalid = false;
+        validationErrors.password = "Veuillez mettre mot de passe"
+    } else if(formData.password.length < 4) {
+        isvalid = false;
+        validationErrors.password = "Votre mot de passe est trop courte, veuillez mettre au moin 4 charactères"
+    }
+
+
+
+    axios.get('http://localhost:6789/users')
+    .then(result => {
+        result.data.map(user => {
+            if(user.email === formData.email) {
+                if(user.password === formData.password) {
+                    alert ("vous êtes connecté")
+                    navigate('/')
+                } else {
+                    isvalid = false;
+                    validationErrors.password = "Votre mot de passe ou email est incorrect"
+                }
+
+            } else if(formData.email !== "") {
+                isvalid = false;
+                validationErrors.email = "Votre mot de passe ou email est incorrect"
+            }
+        })
+        setErrors(validationErrors)
+        setValid(isvalid)
+    })
+    .catch(err => console.log(err))
 }
 
 
-export default Login
+  return (
+    <div classname="formulaire">
+      <form
+        className="container-fluid d-flex justify-content-center w-50 bg-light border border-primary border-5 rounded-5 shadow-lg my-4"
+        onSubmit={handleSubmit}
+      >
+        <div className="rounded-5 py-4 px-5 w-100 ">
+          <h1 className="card-title h1 m-3 border-bottom border-primary border-4 text-center text-primary">
+            <b>Connexion</b>
+          </h1>
+          <div className="row mx-4 my-5">
+            <div className="col-md-12 mt-4">
+              <label className="form-label" htmlFor="email">
+                Adresse email
+              </label>
+              <input
+                className="form-control border border-secondary border-3 rounded-3"
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Entrez votre email"
+                onChange={(event) =>
+                  setFormData({ ...formData, email: event.target.value })
+                }
+              />
+              {valid ? (
+                <></>
+              ) : (
+                <span className="text-danger"> {errors.email} </span>
+              )}
+            </div>
+            <div className="col-md-12 mt-4">
+              <label className="form-label" htmlFor="password">
+                Mot de passe
+              </label>
+              <input
+                className="form-control border border-secondary border-3 rounded-3"
+                type="text"
+                id="password"
+                name="password"
+                placeholder="Entrez votre mot de passe"
+                onChange={(event) =>
+                  setFormData({ ...formData, password: event.target.value })
+                }
+              />
+              {valid ? (
+                <></>
+              ) : (
+                <span className="text-danger"> {errors.password} </span>
+              )}
+            </div>
+            <div className="d-flex flex-column flex-md-row gap-3 justify-content-between">
+              <button className="btn btn-primary btn-lg rounded-5 px-4 flex-fill mt-5"type="submit">Se connecter</button>
+            </div>
+            <p className="text-center small mt-1 text-secondary">
+              Si vous n'aviez pas de compte,<Link to="/registration"> Crée votre compte </Link>{" "}
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
